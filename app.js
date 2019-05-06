8e8ad1f6a34b5c9aded4a08dbfdec06843dc2837
mohaldev <saurabhmohal@gmail.com>
2019-05-06 22:42:37 +0100

    first draft

--- app.js
+++ app.js
@@ -1,15 +1,66 @@
-const express = require("express");
+const express = require('express');
 const app = express();
+const path = require('path');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const Campground = require("./models/campground");
 
-app.set("view engine" , "ejs");
+mongoose.connect('mongodb://localhost/my_database', { useNewUrlParser: true });
 
-app.get("/", (req, res) => {
-    res.render("landing");
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get('/', (req, res) => {
+  res.render('landing');
+});
+
+app.get('/campgrounds', (req, res) => {
+  Campground.find({}, function(err, campgrounds) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render('campgrounds', { campgroundsAll: campgrounds });
+    }
+  });
+});
+
+app.post('/campgrounds', (req, res) => {
+  const newCampground = {
+    name: req.body.name,
+    image: req.body.image,
+    desc: req.body.desc
+  };
+  Campground.create(newCampground, (err, campground) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log('Campground created');
+      console.log(campground);
+      res.redirect('campgrounds');
+    }
+  });
+});
+
+app.get('/campgrounds/new', (req, res) => {
+  res.render('new');
+});
+
+app.get('/campgrounds/:id', (req, res) => {
+  const showCampground = req.params.id;
+  Campground.find({ _id: showCampground }, (err, foundCampground) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(foundCampground);
+      res.render('campground', { campground: foundCampground });
+    }
+  });
 });
 
 const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => {
-    console.log("Server Working")
+  console.log('Server Working');
 });
-
